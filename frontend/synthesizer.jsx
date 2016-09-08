a0f5435381b9a31f2fce8f861d4980be1feb5c4b
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-07 17:59:04 -0700

    added webpack

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<div>testing</div>, document.getElementById("root"));
 });
