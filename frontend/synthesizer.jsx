97ddb3806deac2d5d03c8ea918c7d603eb2a8391
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-07 20:17:34 -0700

    added key constants

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Note from './util/note';
+import TONES from './util/tones';
+import NOTE_NAMES from './util/tones';
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<div>testing</div>, document.getElementById("root"));
 });
+
+// window.note = Note;
+// window.tones = TONES;
+// window.note_names = NOTE_NAMES;
---
+++ frontend/util/tones.js
@@ -0,0 +1,9 @@
+export const TONES = {
+  C5: 523.25,
+  D5: 587.33,
+  E5: 659.25,
+  F5: 698.46,
+  G5: 783.99
+};
+
+export const NOTE_NAMES = Object.keys(TONES);
