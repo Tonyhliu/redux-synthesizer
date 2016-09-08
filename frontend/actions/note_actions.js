89b48b6851ddf429d5872f75a58de85d9bbccf54
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-07 20:39:25 -0700

    added notes actions for store

---
+++ frontend/actions/note_actions.js
@@ -0,0 +1,16 @@
+export const NotesConstants = {
+  KEY_PRESSED: "KEY_PRESSED",
+  KEY_RELEASED: "KEY_RELEASED"
+};
+
+// add key as a property to the action to let store know which key to remove
+// from its notes array
+export const keyPressed = (key) => ({
+  type: NotesConstants.KEY_PRESSED,
+  key
+});
+
+export const keyReleased = (key) => ({
+  type: NotesConstants.KEY_RELEASED,
+  key
+});
