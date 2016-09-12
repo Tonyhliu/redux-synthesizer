48e41e9a5ddedc2166381daed39f3022361734c7
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-11 19:24:40 -0700

    notes playing properly via listeners

--- frontend/components/synth/synth.jsx
+++ frontend/components/synth/synth.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import Note from '../../util/note';
 import { NOTE_NAMES, TONES } from '../../util/tones';
+import $ from 'jquery';
 
 // const Synth = () => (
 //   <div>Synth</div>
 // );
 
-window.note = Note;
-
 class Synth extends React.Component {
   constructor(props) {
     super(props);
-    let noteNames = NOTE_NAMES;
-    let tones = TONES;
-    this.notes = noteNames.map(note => {
-      new window.note(tones[note]); // an array of the Note instances
+    // let noteNames = NOTE_NAMES; // WHAT? Async?
+    // let tones = TONES; // WHAT? Async?
+    this.notes = NOTE_NAMES.map(note => new Note(TONES[note])); // array of the Note instances
+  }
+
+  componentDidMount() {
+    $(document).on('keydown', e => this.onKeyDown(e));
+    $(document).on('keyup', e => this.onKeyUp(e));
+  }
+
+  playNotes() {
+    NOTE_NAMES.forEach((note, idx) => {
+      
+      // play notes present in state
+      if (this.props.notes.indexOf(note) !== -1) {
+        this.notes[idx].start();
+      } else {
+        this.notes[idx].stop();
+      }
     });
   }
 
+  onKeyDown(e) {
+    this.props.keyPressed(e.key);
+  }
+
+  onKeyUp(e) {
+    this.props.keyReleased(e.key);
+  }
+
   render() {
+    this.playNotes();
+
     return(
       <ul>
-        {this.notes}
-      </ul>
-    );
+        {Object.keys(this.notes).map((note) => {
+            return <li>{note}</li>;
+          })}
+      </ul>);
   }
-}
-
+};
 
 export default Synth;
